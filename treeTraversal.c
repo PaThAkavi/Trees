4eccc5070311133ec6b11d34e65b821cfdf1f10e
Avaneesh <pathakavaneesh@gmail.com>
2020-03-06 20:37:59 +0530

    Added new function

--- treeTraversal.c
+++ treeTraversal.c
@@ -7,25 +7,45 @@ typedef struct Node {
     struct Node* right;
 }node;
 
-node* root = NULL;
+node* insert(node* root, int val){
+    if(!root){
+        node* newnode = (node*)malloc(sizeof(node));
+        newnode->data = val;
+        newnode->left = NULL;
+        newnode->right =  NULL;
+        root = newnode;
+    } else {
+        if(val > root->data){
+            root->right = insert(root->right, val);
+        } else{
+            root->left = insert(root->left, val);
+        }
+    }
+    return root;
+}
+
+void inorder(node* root){
+    if(root){
+        inorder(root->left);
+        printf("%d\n", root->data);
+        inorder(root->right);
+    }
+}
 
 void display(node* root){
     if(root){
-        printf("%d\n", root->data);
+        printf("%d ", root->data);
     } else{
         return;
     }
 }
 
 int main(){
-    node* newnode = (node*)malloc(sizeof(node));
     int r;
     printf("Enter the root node:\n");
     scanf("%d", &r);
-    newnode->data = r;
-    newnode->left = NULL;
-    newnode->right =  NULL;
-    root = newnode;
-    display(newnode);
+    node* root = (node*)malloc(sizeof(node));
+    insert(root, r);
+    inorder(root);
     return 0;
 }
